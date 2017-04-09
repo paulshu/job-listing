63df17c163e50f746829415a6f44c34bfc23e6f1
paulshu <paulshu18@gmail.com>
2017-04-09 17:54:53 +0800

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
   namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
   end
 
+  resources :jobs
   root 'jobs#index'
 end
